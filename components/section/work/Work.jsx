623f578e3bb268bc87db5e237190d4e5c4930768
April Tao <95392008+apriltaoyvr@users.noreply.github.com>
2022-05-15 13:47:23 -0700

    Adding Levels 101 to Work

--- components/section/work/Work.jsx
+++ components/section/work/Work.jsx
@@ -23,19 +23,24 @@ const Projects = () => {
         <motion.h4 variants={slideItem}>{t('projects.subtitle')}</motion.h4>
       </motion.hgroup>
       <ProjectWrapper variants={slideItem}>
-          <VideoLink
-            href='https://www.ambrosialab.it'
-            video='https://res.cloudinary.com/dsbhmynmq/video/upload/v1652570265/April%20Tao/Projects/AmbrosiaLab/2022-05-14_16-16-35_skywsw.mp4'
-          />
-          <VideoLink
-            href='https://www.theripper93.com'
-            video='https://res.cloudinary.com/dsbhmynmq/video/upload/v1652570268/April%20Tao/Projects/theripper93/2022-05-14_16-15-31_bdlkkc.mp4'
-          />
-          <ImageLink
-            title='Dracula Foundry'
-            href='https://github.com/apriltaoyvr/dracula-foundry'
-            image='https://user-images.githubusercontent.com/95392008/147394179-6777840b-3554-4e9c-b4c3-50cd99616ee2.png'
-          />
+        <VideoLink
+          href='https://www.ambrosialab.it'
+          video='https://res.cloudinary.com/dsbhmynmq/video/upload/v1652570265/April%20Tao/Projects/AmbrosiaLab/2022-05-14_16-16-35_skywsw.mp4'
+        />
+        <VideoLink
+          href='https://www.theripper93.com'
+          video='https://res.cloudinary.com/dsbhmynmq/video/upload/v1652570268/April%20Tao/Projects/theripper93/2022-05-14_16-15-31_bdlkkc.mp4'
+        />
+        <VideoLink
+          title='Levels 101'
+          href='https://github.com/apriltaoyvr/levels-101'
+          video='https://user-images.githubusercontent.com/95392008/147842446-6e528d8a-11cf-43c1-badb-0619289acc0f.mp4'
+        />
+        <ImageLink
+          title='Dracula Foundry'
+          href='https://github.com/apriltaoyvr/dracula-foundry'
+          image='https://user-images.githubusercontent.com/95392008/147394179-6777840b-3554-4e9c-b4c3-50cd99616ee2.png'
+        />
       </ProjectWrapper>
     </WorkWrapper>
   );
