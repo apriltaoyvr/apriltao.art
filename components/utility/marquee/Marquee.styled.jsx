3a2013bb2a19d075715b58f5d8a2b408e49af0a0
April Tao <95392008+apriltaoyvr@users.noreply.github.com>
2022-05-18 15:18:55 -0700

    Marquee

--- components/utility/marquee/Marquee.styled.jsx
+++ components/utility/marquee/Marquee.styled.jsx
@@ -18,15 +18,22 @@ export const TextWrapper = styled(motion.hgroup)`
   flex-direction: row;
   justify-content: center;
   width: 100%;
-  gap: 2rem;
-  padding: 0 2rem 0 2rem;
+  padding: 0 2rem;
+  gap: 1rem;
   text-transform: uppercase;
+  @media screen and (min-width: 1600px) {
+    gap: 2rem;
+  }
 `;
 
 export const SolidText = styled(motion.h1)`
   font-size: 5.5rem;
   overflow: hidden;
   color: var(--accent);
+
+  @media screen and (min-width: 1600px) {
+    font-size: 9rem;
+  }
 `;
 
 export const HollowText = styled(motion.h1)`
@@ -34,4 +41,8 @@ export const HollowText = styled(motion.h1)`
   overflow: hidden;
   color: transparent;
   -webkit-text-stroke: 1.5px var(--accent);
+
+  @media screen and (min-width: 1600px) {
+    font-size: 9rem;
+  }
 `;
