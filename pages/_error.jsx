c1dd58375ab155e45b0c4222fee5e58409c8a15d
April Tao <95392008+apriltaoyvr@users.noreply.github.com>
2022-05-21 19:17:18 -0700

    404 Page

---
+++ pages/_error.jsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const StyledError = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100vh;
+  width: 100vw;
+`;
+
+export default function Error() {
+  return (
+    <StyledError>
+      <h1>Page Not Found</h1>
+      <Link href='/'>
+        <a>Go back home</a>
+      </Link>
+    </StyledError>
+  );
+}
