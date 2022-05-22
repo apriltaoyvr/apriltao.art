3570f2e87794f81a07caebfcaa27ad47c2e402c5
April Tao <95392008+apriltaoyvr@users.noreply.github.com>
2022-05-22 13:28:20 -0700

    Link image preview

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,22 +1,33 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <meta name="description" content="Web design, development, and modelling" />
-          <link rel="icon" href="/static/image/heart.svg" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
-          <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap" rel="stylesheet" />
+          <meta
+            name='description'
+            content='Web design, development, and modelling'
+          />
+          <link rel='icon' href='/static/image/heart.svg' />
+          <link rel='image_src' href='/static/image/preview.png' />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin='true'
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap'
+            rel='stylesheet'
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
