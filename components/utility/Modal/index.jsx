37db7c0647e58353cc4e2bf0cb6875a336207010
April Tao <95392008+apriltaoyvr@users.noreply.github.com>
2022-05-30 18:19:27 -0700

    Modal refactor

--- components/utility/Modal/index.jsx
+++ components/utility/Modal/index.jsx
@@ -1,27 +1,33 @@
-import { motion } from 'framer-motion';
+import { AnimatePresence, MotionConfig, motion } from 'framer-motion';
 import { StyledModal, WindowBar, WindowIcon, StyledH3 } from './index.styled';
 import Backdrop from './Backdrop';
 
-const Modal = ({ handleClose, title, children }) => {
+const Modal = ({ modalOpen, handleClose, title, children }) => {
   return (
-    <Backdrop onClick={handleClose}>
-      <StyledModal
-        onClick={(e) => e.stopPropagation()}
-        initial={{ y: '-75%', opacity: 0 }}
-        animate={{ y: '0', opacity: 1 }}
-        exit={{ y: '75%', opacity: 0 }}
-        transition={{ type: 'spring', bounciness: 0, duration: 0.66 }}
-      >
-        <WindowBar>
-          <motion.div whileTap={{ scale: 0.95 }} onClick={handleClose}>
-            <WindowIcon icon='fa-solid fa-x' size='xl' />
-          </motion.div>
-          <StyledH3>{title}</StyledH3>
-          <p style={{ userSelect: 'none' }}>⠀⠀</p>
-        </WindowBar>
-        {children}
-      </StyledModal>
-    </Backdrop>
+    <MotionConfig>
+      <AnimatePresence>
+        {modalOpen && (
+          <Backdrop onClick={handleClose}>
+            <StyledModal
+              onClick={(e) => e.stopPropagation()}
+              initial={{ y: '-75%', opacity: 0 }}
+              animate={{ y: '0', opacity: 1 }}
+              exit={{ y: '75%', opacity: 0 }}
+              transition={{ type: 'spring', bounciness: 0, duration: 0.66 }}
+            >
+              <WindowBar>
+                <motion.div whileTap={{ scale: 0.95 }} onClick={handleClose}>
+                  <WindowIcon icon='fa-solid fa-x' size='xl' />
+                </motion.div>
+                <StyledH3>{title}</StyledH3>
+                <p style={{ userSelect: 'none' }}>⠀⠀</p>
+              </WindowBar>
+              {children}
+            </StyledModal>
+          </Backdrop>
+        )}
+      </AnimatePresence>
+    </MotionConfig>
   );
 };
 
--- components/utility/Navbar/ThemeMenu/index.jsx
+++ components/utility/Navbar/ThemeMenu/index.jsx
@@ -1,6 +1,6 @@
 /* Libs */
 import { useState } from 'react';
-import { AnimatePresence, MotionConfig, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { useTranslation } from 'next-i18next';
 
 /* Components */
@@ -26,45 +26,41 @@ const ThemeMenu = ({ setTheme }) => {
   };
 
   return (
-    <MotionConfig>
-      <AnimatePresence>
-        <motion.div
-          whileTap={{ scale: 0.95 }}
-          onClick={() => (modalOpen ? close() : open())}
-        >
-          <StyledIcon icon='fa-solid fa-palette' size='xl' />
-        </motion.div>
-        {modalOpen && (
-          <Modal
-            modalOpen={modalOpen}
-            handleClose={close}
-            title={t('utility.themes')}
-            key='theme modal'
-          >
-            <ThemeGrid>
-              <ThemeItem
-                setTheme={setTheme}
-                palette={dracula}
-                background='hsl(234, 14%, 13%)'
-                border='hsl(250, 100%, 75%)'
-              />
-              <ThemeItem
-                background='hsl(221, 16%, 19%)'
-                border='hsl(193, 43%, 67%)'
-                setTheme={setTheme}
-                palette={nordDark}
-              />
-              <ThemeItem
-                background='hsl(240, 27%, 18%)'
-                border='hsl(14, 50%, 75%)'
-                setTheme={setTheme}
-                palette={pink}
-              />
-            </ThemeGrid>
-          </Modal>
-        )}
-      </AnimatePresence>
-    </MotionConfig>
+    <>
+      <motion.div
+        whileTap={{ scale: 0.95 }}
+        onClick={() => (modalOpen ? close() : open())}
+      >
+        <StyledIcon icon='fa-solid fa-palette' size='xl' />
+      </motion.div>
+      <Modal
+        modalOpen={modalOpen}
+        handleClose={close}
+        title={t('utility.themes')}
+        key='theme modal'
+      >
+        <ThemeGrid>
+          <ThemeItem
+            setTheme={setTheme}
+            palette={dracula}
+            background='hsl(234, 14%, 13%)'
+            border='hsl(250, 100%, 75%)'
+          />
+          <ThemeItem
+            background='hsl(221, 16%, 19%)'
+            border='hsl(193, 43%, 67%)'
+            setTheme={setTheme}
+            palette={nordDark}
+          />
+          <ThemeItem
+            background='hsl(240, 27%, 18%)'
+            border='hsl(14, 50%, 75%)'
+            setTheme={setTheme}
+            palette={pink}
+          />
+        </ThemeGrid>
+      </Modal>
+    </>
   );
 };
 
